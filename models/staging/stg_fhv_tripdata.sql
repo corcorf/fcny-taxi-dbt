163c80f499ae257d09fb7f124d20fbc26441d34e
Flann Corcoran <flann@posteo.net>
2022-02-27 19:21:15 +0000

    add yellow, green and fhv trips to staging

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='view') }}
+
+select
+    -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(dispatching_base_num as integer) as dispatching_base_num,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    SR_Flag,
+    Affiliated_base_number
+from `fcny-taxi`.`trips_data_all`.`fhv_tripdata`
+where dispatching_base_num IS NOT NULL
+
+{% if var("is_test_run", default=true) %}
+
+limit 100
+
+{% endif %}
